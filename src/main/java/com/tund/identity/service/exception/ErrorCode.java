e656ce4ca303149178bd73bd156610c58a940738
Ngo Duc Tu <ductutgt@gmail.com>
2025-12-26 15:08:00 +0700

    attributes from annotation

--- src/main/java/com/tund/identity/service/exception/ErrorCode.java
+++ src/main/java/com/tund/identity/service/exception/ErrorCode.java
@@ -9,12 +9,12 @@ public enum ErrorCode {
     DEFAULT(999, "Default exception error", HttpStatus.INTERNAL_SERVER_ERROR),
     FAILKEY(900, "Fail enum key", HttpStatus.BAD_REQUEST),
     USER_EXISTED(101, "User existed", HttpStatus.BAD_REQUEST),
-    INVALID_USERNAME(102, "Username must be at least 3 charactors", HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(103, "Password must be at least 8 charactors", HttpStatus.BAD_REQUEST),
+    INVALID_USERNAME(102, "Username must be at least {min} charactors", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(103, "Password must be at least {min}} charactors", HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND(104, "User not found", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(105, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     ACCESS_DENY(106, "Access deny", HttpStatus.FORBIDDEN),
-    INVALID_DOB(107, "Invalid birth day", HttpStatus.BAD_REQUEST);
+    INVALID_DOB(107, "User must be lagre than {min}", HttpStatus.BAD_REQUEST);
 
     private int code;
     private String message;
--- src/main/java/com/tund/identity/service/exception/GlobalException.java
+++ src/main/java/com/tund/identity/service/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.tund.identity.service.exception;
 
 import com.tund.identity.service.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,9 +9,19 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 @Slf4j
 public class GlobalException {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
+    }
 
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
@@ -25,16 +36,24 @@ public class GlobalException {
     ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         String enumKey = exception.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.FAILKEY;
-
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constrainViolation = exception.getBindingResult()
+                    .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constrainViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
+
         } catch (IllegalArgumentException e) {
 
         }
 
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ? mapAttribute(errorCode.getMessage(), attributes) :
+                errorCode.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
